b99ca6105ac4dee62c7bdaf26e4f737d63b63ccf
SgtSteiner <futitotal@gmail.com>
2020-12-07 21:25:36 +0100

    Solve day 3

--- 2015/Day 03/solve.py
+++ 2015/Day 03/solve.py
@@ -1,40 +1,7 @@
-"""
---- Day 3: Perfectly Spherical Houses in a Vacuum ---
-
-Santa is delivering presents to an infinite two-dimensional grid of houses.
-
-He begins by delivering a present to the house at his starting location,
-and then an elf at the North Pole calls him via radio and tells him where to move next.
-Moves are always exactly one house to the north (^), south (v), east (>), or west (<).
-After each move, he delivers another present to the house at his new location.
-
-However, the elf back at the north pole has had a little too much eggnog,
-and so his directions are a little off, and Santa ends up visiting some houses more than once.
-How many houses receive at least one present?
-
-For example:
-
-. > delivers presents to 2 houses: one at the starting location, and one to the east.
-. ^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
-. ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
-
---- Part Two ---
-
-The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa,
-to deliver presents with him.
-
-Santa and Robo-Santa start at the same location (delivering two presents to the same starting house),
-then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same
-script as the previous year.
-
-This year, how many houses receive at least one present?
-
-For example:
-
-. ^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
-. ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
-. ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
-"""
+def read_input():
+    # Read input data
+    with open("input.txt", 'r') as f:
+        return list(f.read())
 
 
 def move_house(rumbo, x_coord, y_coord):
@@ -49,28 +16,32 @@ def move_house(rumbo, x_coord, y_coord):
     return x_coord, y_coord
 
 
-datos = list(input())
-
-""" --- Part One --- """
-houses = [(0, 0)]           # Se inicia la ruta en las coordenadas (0, 0)
-x_santa = y_santa = 0
-for dato in datos:
-    x_santa, y_santa = move_house(dato, x_santa, y_santa)
-    houses.append((x_santa, y_santa))
-
-print("Total houses: {0}".format(len(set(houses))))
-
-""" --- Part Two --- """
-houses = [(0, 0)]           # Se inicia la ruta en las coordenadas (0, 0)
-x_santa = y_santa = x_robo = y_robo = 0
-movement = 0
-for dato in datos:
-    movement += 1
-    if movement % 2 != 0:
-        x_santa, y_santa = move_house(dato, x_santa, y_santa)
+def house_santa(moves):
+    houses = [(0, 0)]  # Se inicia la ruta en las coordenadas (0, 0)
+    x_santa = y_santa = 0
+    for move in moves:
+        x_santa, y_santa = move_house(move, x_santa, y_santa)
         houses.append((x_santa, y_santa))
-    else:
-        x_robo, y_robo = move_house(dato, x_robo, y_robo)
-        houses.append((x_robo, y_robo))
+    return len(set(houses))
 
-print("Total houses Robo-Santa: {0}".format(len(set(houses))))
+
+def house_robosanta(moves):
+    houses = [(0, 0)]  # Se inicia la ruta en las coordenadas (0, 0)
+    x_santa = y_santa = x_robo = y_robo = 0
+    movement = 0
+    for move in moves:
+        movement += 1
+        if movement % 2 != 0:
+            x_santa, y_santa = move_house(move, x_santa, y_santa)
+            houses.append((x_santa, y_santa))
+        else:
+            x_robo, y_robo = move_house(move, x_robo, y_robo)
+            houses.append((x_robo, y_robo))
+    return len(set(houses))
+
+
+if __name__ == "__main__":
+    datos = read_input()
+
+    print(f"Total houses Santa: {house_santa(datos)}")
+    print(f"Total houses Robo Santa: {house_robosanta(datos)}")
--- 2015/Day 03/test_day03.py
+++ 2015/Day 03/test_day03.py
@@ -1,16 +1,18 @@
 import unittest
 
-from solve import feet_paper, feet_ribbon
+from solve import house_santa, house_robosanta
 
 
 class MyTestCase(unittest.TestCase):
-    def test_feet_paper(self):
-        self.assertEqual(feet_paper(["2x3x4"]), 58)
-        self.assertEqual(feet_paper(["1x1x10"]), 43)
+    def test_house_santa(self):
+        self.assertEqual(house_santa(list(">")), 2)
+        self.assertEqual(house_santa(list("^>v<")), 4)
+        self.assertEqual(house_santa(list("^v^v^v^v^v")), 2)
 
-    def test_feet_ribbon(self):
-        self.assertEqual(feet_ribbon(["2x3x4"]), 34)
-        self.assertEqual(feet_ribbon(["1x1x10"]), 14)
+    def test_house_robosanta(self):
+        self.assertEqual(house_robosanta(list("^v")), 3)
+        self.assertEqual(house_robosanta(list("^>v<")), 3)
+        self.assertEqual(house_robosanta(list("^v^v^v^v^v")), 11)
 
 
 if __name__ == '__main__':
